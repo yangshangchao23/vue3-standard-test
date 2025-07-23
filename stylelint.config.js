919f91cbb976b523b7453330d0ba83cbfdacd2a2
yangshangchao <yangshangchao@foresee.com.cn>
2025-07-23 11:02:02 +0800

    fix: 根据codeRabbit代码审查修改代码

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,7 +5,12 @@ export default {
     'stylelint-config-recess-order',
     'stylelint-config-standard-scss',
   ],
-  customSyntax: 'postcss-html',
+  overrides: [
+    {
+      files: ['**/*.{vue,html,scss}'],
+      customSyntax: 'postcss-html',
+    },
+  ],
   rules: {
     // 'string-quotes': 'single',  // 没有这一项规则会报错
     'color-hex-length': 'short',
